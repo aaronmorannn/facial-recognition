30bd4690b26172e455f70a2006417b16850d42e4
ArnasSteponavicius00 <g00361891@gmit.ie>
2020-02-09 17:28:16 +0000

    Updated camera.py to only take a picture on keypress and save to images folder

--- Code-Snippets/camera.py
+++ Code-Snippets/camera.py
@@ -4,27 +4,16 @@ import cv2
 
 capture = cv2.VideoCapture(0)
 
-cv2.namedWindow("Test")
-
-# Access camera and refresh
-if not capture.isOpened():
-    print("Cannot open camera")
-    exit()
 while True:
-    # Capture frame by frame
+    #Show camera window
     ret, frame = capture.read()
-
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame. Exiting ...")
-        break
-    # Code that deals with the frames goes here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #Display resulting frame
-    cv2.imshow('Camera', gray)
-    if cv2.waitKey(1) == ord('q'):
+    cv2.imshow("Take profile picture", frame) 
+    
+    #Save on pressing "Spacebar" then exit
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        cv2.imwrite("./TestImages/usr1.png", frame)
+        cv2.destroyAllWindows()
         break
 
 # When everything done, release the capture
 capture.release()
-cv2.destroyAllWindows()
