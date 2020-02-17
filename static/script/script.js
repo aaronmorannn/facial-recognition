8641e61f4a14d1e760fa7dc583bbf7b8f6349325
ArnasSteponavicius00 <g00361891@gmit.ie>
2020-02-17 11:05:23 +0000

    access camera from browser

--- static/script/script.js
+++ static/script/script.js
@@ -1,8 +1,32 @@
- function goPython(){
-       $.ajax({
-  		     url: "../../python_files/camera.py",
-             context: document.body
-             }).done(function() {
-             alert('finished python script');;
-            });
-  }
+ // function goPython(){
+ //       $.ajax({
+ //  		     url: "../../python_files/camera.py",
+ //             context: document.body
+ //             }).done(function() {
+ //             alert('finished python script');;
+ //            });
+ //  }
+
+ function camera(){
+	// Grab elements, create settings, etc.
+	var video = document.getElementById('video');
+
+	// Get access to the camera!
+	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+	    // Not adding `{ audio: true }` since we only want video now
+	    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+	        //video.src = window.URL.createObjectURL(stream);
+	        video.srcObject = stream;
+	        video.play();
+	    });
+	}
+
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var video = document.getElementById('video');
+
+	// Trigger photo take
+	document.getElementById("snap").addEventListener("click", function() {
+		context.drawImage(video, 0, 0, 640, 480);
+	});
+}
