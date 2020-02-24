8edda9621c5da267bafcf69a3a86fecc3b1bf52b
THOMAS KENNY <G00359994@gmit.ie>
2020-02-24 13:30:21 +0000

    Fixed layout

--- application/static/script/script.js
+++
@@ -1,8 +0,0 @@
- function goPython(){
-       $.ajax({
-  		     url: "../../python_files/camera.py",
-             context: document.body
-             }).done(function() {
-             alert('finished python script');;
-            });
-  }
