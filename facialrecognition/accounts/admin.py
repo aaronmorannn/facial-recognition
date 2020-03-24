1ff35037f177368586eaa5f2899a6549482e2f13
Aaron Moran <aaronmorann@protonmail.com>
2020-03-24 15:55:47 +0000

    remove FLASK

--- facialrecognition/accounts/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from accounts.models import UserProfile
-
-admin.site.register(UserProfile)
--- facialrecognition/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def home(request):
-    numbers = [1,2,3,4,5]
-    name ='Aaron Moran'
-
-    args = {'myName': name, 'numbers': numbers}
-    return render(request, 'accounts/home.html', args)
